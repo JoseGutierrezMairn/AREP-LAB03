1ba9f697e22b533ce949ad99e73ab6ef16d7568b
Jose Gutierrez Marin <andres.gutierrez-m@mail.escuelaing.edu.co>
2021-02-09 12:12:15 -0500

    Procfile ready

--- src/main/java/edu/escuelaing/arep/app/httpServer.java
+++ src/main/java/edu/escuelaing/arep/app/httpServer.java
@@ -77,6 +77,6 @@ public class httpServer {
 		if (System.getenv("PORT") != null) {
 			return Integer.parseInt(System.getenv("PORT"));
 		}
-		return 35000; //returns default port if heroku-port isn't set(i.e. on localhost)
+		return 35100; //returns default port if heroku-port isn't set(i.e. on localhost)
 	}
 }
